248ce1984b32f7cb06448bf2b41413ef38e0b59e
Tim Kuminecz <tkuminecz@gmail.com>
2023-11-11 02:17:02 -0500

    save wip

--- scripts/debt_payoff.ts
+++ scripts/debt_payoff.ts
@@ -1,6 +1,7 @@
-import colors from "colors";
 import * as ynab from "ynab";
-import { table } from "table";
+import colors from "colors";
+import lodash from "lodash";
+import { createStream as createTableStream, table } from "table";
 import { Redis } from "@upstash/redis";
 import { get_min_payment, my_accounts } from "./accounts";
 
@@ -37,7 +38,7 @@ async function getCached<T>(
 
 async function fetchAccounts(): Promise<ynab.Account[]> {
   const cache_key = `ynab:${budget_id}:accounts:2`;
-  const accountsResponse = await getCached(cache_key, 1800, async () => {
+  const accountsResponse = await getCached(cache_key, 3600, async () => {
     return ynabApi.accounts.getAccounts(budget_id);
   });
   return accountsResponse.data.accounts.filter(
@@ -47,7 +48,7 @@ async function fetchAccounts(): Promise<ynab.Account[]> {
 
 async function fetchCategories(): Promise<ynab.Category[]> {
   const cache_key = `ynab:${budget_id}:categories:1`;
-  const categoriesResponse = await getCached(cache_key, 1800, async () => {
+  const categoriesResponse = await getCached(cache_key, 3600, async () => {
     return ynabApi.categories.getCategories(budget_id);
   });
   return categoriesResponse.data.category_groups.flatMap(
@@ -60,7 +61,7 @@ async function fetchCategoryMonth(
   month: string
 ): Promise<ynab.Category> {
   const cache_key = `ynab:${budget_id}:category_month:${category_id}:${month}:1`;
-  const category = await getCached(cache_key, 1800, () =>
+  const category = await getCached(cache_key, 3600, () =>
     ynabApi.categories.getMonthCategoryById(budget_id, month, category_id)
   );
   return category.data.category;
@@ -86,17 +87,30 @@ function* generate_months() {
     const month_str = month < 10 ? "0" + month : month;
     yield `${year}-${month_str}-01`;
 
-    date.setMonth(date.getMonth() - 1); // Move to the previous month
+    date.setMonth(date.getMonth() + 1); // Move to the next month
   }
 }
 
-interface Debt {
-  name: string;
-  true_balance: number;
-  min_payment: number;
+function calc_total_debt(debts: PayoffDebt[]): number {
+  return debts.reduce((acc, debt) => {
+    return acc + debt.true_balance;
+  }, 0);
 }
 
-interface CreditCard extends Debt {
+function apply_payment_to_debt(debt: PayoffDebt, payment: number): PayoffDebt {
+  return {
+    ...debt,
+    true_balance: debt.true_balance - payment,
+  };
+}
+
+interface BaseDebt {
+  name: string;
+  true_balance: number;
+  interest_rate: number;
+}
+
+interface CreditCard extends BaseDebt {
   name: string;
   cleared_balance: number;
   uncleared_balance: number;
@@ -105,10 +119,20 @@ interface CreditCard extends Debt {
   from_prev_month: number;
   spending: number;
   for_payment: number;
+  min_payment: number;
+  snowball: number;
 }
 
-interface Loan extends Debt {
+interface Loan extends BaseDebt {
+  min_payment: number;
   balance: number;
+}
+
+interface PayoffDebt extends BaseDebt {
+  monthly_interest: number;
+  payment: number;
+  snowball: number;
+  payoff_n: number;
 }
 
 async function main() {
@@ -135,6 +159,7 @@ async function main() {
             throw new Error(`No my_accounts info for ${account.name}`);
           }
 
+          const min_payment = get_min_payment(my_account_info, account.balance);
           return {
             name: account.name,
             cleared_balance: account.cleared_balance,
@@ -144,8 +169,10 @@ async function main() {
             from_prev_month: prevMonthCategory.balance,
             spending: category.activity,
             for_payment: category.balance,
-            min_payment: get_min_payment(my_account_info, account.balance),
+            min_payment,
             true_balance: account.balance + category.balance,
+            interest_rate: my_account_info.interest_rate,
+            snowball: category.budgeted,
             // category,
           };
         })
@@ -168,7 +195,9 @@ async function main() {
       spending: acc.spending + card.spending,
       for_payment: acc.for_payment + card.for_payment,
       min_payment: acc.min_payment + card.min_payment,
+      interest_rate: acc.interest_rate + card.interest_rate,
       true_balance: acc.true_balance + card.true_balance,
+      snowball: acc.snowball + card.snowball,
     };
   });
   ccTotals.name = "Totals";
@@ -180,8 +209,9 @@ async function main() {
       [
         creditCardColumns.map((column) => colors.dim(column)),
         ...creditCards.concat([ccTotals]).map((row) =>
-          Object.values(row).map((value) => {
-            if (typeof value === "number") return fmt(value);
+          Object.entries(row).map(([key, value]) => {
+            if (typeof value === "number" && key !== "interest_rate")
+              return fmt(value);
             return value;
           })
         ),
@@ -210,7 +240,6 @@ async function main() {
             ].includes(account.type) && !account.closed
         )
         .map(async (account) => {
-          //   console.log("->loan", account);
           return {
             name: account.name,
             balance: account.balance,
@@ -218,6 +247,9 @@ async function main() {
             min_payment: Object.values(
               account.debt_minimum_payments ?? { payment: 0 }
             )[0],
+            interest_rate:
+              Object.values(account.debt_interest_rates ?? { rate: 0 })[0] /
+              100000,
           };
         })
     )
@@ -233,23 +265,175 @@ async function main() {
       balance: acc.balance + loan.balance,
       true_balance: acc.true_balance + loan.true_balance,
       min_payment: acc.min_payment + loan.min_payment,
+      interest_rate: acc.interest_rate + loan.interest_rate,
     };
   });
 
   console.log("Loans");
   const loanColumns = Object.keys(loans[0]);
   console.log(
-    table([
-      loanColumns.map((column) => colors.dim(column)),
-      ...loans.concat([loanTotals]).map((row) =>
-        Object.values(row).map((value) => {
-          if (typeof value === "number") return fmt(value);
-          return value;
-        })
-      ),
-    ])
+    table(
+      [
+        loanColumns.map((column) => colors.dim(column)),
+        ...loans.concat([loanTotals]).map((row) =>
+          Object.entries(row).map(([key, value]) => {
+            if (typeof value === "number" && key !== "interest_rate")
+              return fmt(value);
+            return value;
+          })
+        ),
+      ],
+      {
+        columns: loanColumns.map(() => {
+          return {
+            alignment: "right",
+          };
+        }),
+      }
+    )
   );
 
+  const DEBT_PAYOFF_SORT = (a: PayoffDebt, b: PayoffDebt) => {
+    // pay off my interest rate
+    return a.monthly_interest - b.monthly_interest;
+  };
+
+  const debts: PayoffDebt[] = [...creditCards, ...loans]
+    .map((debt) => {
+      return {
+        name: debt.name,
+        interest_rate: debt.interest_rate,
+        payment: debt.min_payment,
+        true_balance: debt.true_balance,
+        monthly_interest: debt.true_balance * (debt.interest_rate / 12),
+        snowball: (debt as any).snowball ?? 0,
+        payoff_n: Infinity,
+      } satisfies PayoffDebt;
+    })
+    .filter((d) => d.true_balance < 0)
+    .sort(DEBT_PAYOFF_SORT);
+  console.log("All Debts");
+  if (debts.length === 0) {
+    console.log(colors.green("You are debt free!"));
+    return;
+  }
+  const debtColumns = Object.keys(debts[0]);
+  console.log(
+    table(
+      [
+        debtColumns.map((column) => colors.dim(column)),
+        ...debts.map((row) =>
+          Object.entries(row).map(([key, value]) => {
+            if (typeof value === "number" && key !== "interest_rate")
+              return fmt(value);
+            return value;
+          })
+        ),
+      ],
+      {
+        columns: debtColumns.map(() => {
+          return {
+            alignment: "right",
+          };
+        }),
+      }
+    )
+  );
+
+  const total_req_payments = debts.reduce((acc, debt) => {
+    return acc + debt.payment;
+  }, 0);
+  const total_snowball = debts.reduce((acc, debt) => {
+    return acc + debt.snowball;
+  }, 0);
+  const total_debt_payments = total_req_payments + total_snowball;
+
+  console.log(
+    table(
+      [
+        ["total_req_payments", "total_snowball", "total_debt_payments"],
+        [
+          fmt(total_req_payments),
+          fmt(total_snowball),
+          fmt(total_debt_payments),
+        ],
+      ],
+      {
+        columns: [
+          {
+            alignment: "right",
+          },
+          {
+            alignment: "right",
+          },
+          {
+            alignment: "right",
+          },
+        ],
+      }
+    )
+  );
+
+  /* Payoff plan */
+  const payoffRows: (string | number)[][] = [];
+  const payoffDebts = lodash.cloneDeep(debts);
+
+  let n = 0;
+  for (const month of generate_months()) {
+    payoffRows.push([month, ...payoffDebts.map((debt) => debt.true_balance)]);
+
+    payoffDebts.forEach((debt, i) => {
+      const { payment } = debt;
+      debt.true_balance = Math.min(0, debt.true_balance + payment);
+      if (debt.true_balance === 0 && debts[i].payoff_n > n) {
+        debts[i].payoff_n = n;
+      }
+    });
+    const total_debt = calc_total_debt(payoffDebts);
+
+    n += 1;
+    if (total_debt >= 0) {
+      break;
+    }
+  }
+
+  /* payoff plan by payoff date */
+  const debts_by_payoff_date = lodash.cloneDeep(debts).sort((a, b) => {
+    return a.payoff_n - b.payoff_n;
+  });
+  const payoff_date_rows: (string | number)[][] = [];
+  for (const month of generate_months()) {
+    payoff_date_rows.push([
+      month,
+      ...debts_by_payoff_date.map((debt) => debt.true_balance),
+    ]);
+    debts_by_payoff_date.forEach((debt) => {
+      const { payment } = debt;
+      debt.true_balance = Math.min(0, debt.true_balance + payment);
+    });
+    const total_debt = calc_total_debt(debts_by_payoff_date);
+    if (total_debt >= 0) {
+      break;
+    }
+  }
+
   console.log("Payoff Plan");
+  console.log(
+    table(
+      [
+        ["month", ...debts_by_payoff_date.map((debt) => debt.name)],
+        ...payoff_date_rows.map((row) =>
+          row.map((value) => (typeof value === "number" ? fmt(value) : value))
+        ),
+      ],
+      {
+        columns: debts_by_payoff_date.map(() => {
+          return {
+            alignment: "right",
+          };
+        }),
+      }
+    )
+  );
 }
 main();
